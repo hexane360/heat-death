e0880ce000ddc7b9cdb418e60fa572407f4a9474
hexane360 <colin@gilgenbach.net>
2020-04-27 10:17:35 -0600

    Fixed a lot

--- scroll.js
+++ scroll.js
@@ -1,22 +1,36 @@
-window.addEventListener('scroll', scrollHandler);
+var title_h = 2000;
+var first_h = 2000;
+var second_h = 2000;
 
-function centerOf(element) {
-	let r = element.getBoundingClientRect();
-	return r.y + r.height / 2;
+function centerOf(elem) {
+	let style = window.getComputedStyle(elem, null);
+	let top = elem.getBoundingClientRect().top + window.scrollY;
+	return top + parseFloat(style.getPropertyValue("padding-top")) + parseFloat(style.getPropertyValue("height")) / 2;
 }
 
-function scrollHandler(e) {
-	let marker = document.getElementById('marker');
-	let total_height = $(document).height() - window.innerHeight;
+function updateHeights() {
+	title_h = centerOf(document.getElementById('title'));
+	first_h = centerOf(document.getElementById('first'));
+	second_h = centerOf(document.getElementById('second'));
+
+	document.getElementById('titlepos').innerHTML = title_h;
+	document.getElementById('firstpos').innerHTML = first_h;
+	document.getElementById('secondpos').innerHTML = second_h;
+}
+
+function updatePos() {
+	let total_height = document.body.clientHeight - window.innerHeight;
 	document.getElementById('scroll').innerHTML = window.scrollY + '/' + total_height;
 	let svg_height = document.getElementById('graph').scrollHeight;
-	marker.setAttribute('y1', (svg_height * window.scrollY / total_height));
-	marker.setAttribute('y2', (svg_height * window.scrollY / total_height));
 
-	document.getElementById('firstpos').innerHTML = centerOf(document.getElementById('first')) - window.innerHeight / 2;
-	document.getElementById('secondpos').innerHTML = centerOf(document.getElementById('second')) - window.innerHeight / 2;
+	let marker = document.getElementById('marker');
+	marker.setAttribute('y1', svg_height * window.scrollY / total_height);
+	marker.setAttribute('y2', svg_height * window.scrollY / total_height);
 }
 
 window.onload = function() {
-	scrollHandler(null);
+	updateHeights();
+	updatePos();
 };
+window.addEventListener('scroll', function(e) { updatePos(); });
+window.addEventListener('resize', function(e) { updateHeights(); });
